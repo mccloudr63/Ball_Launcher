cd806253898a540a1beab97ab54f51ff89f3c83e
Ryan McCloud <rmccloud63@gmail.com>
2022-03-01 17:01:45 -0500

    Add ball_launch framework (still bugs to work out with calculation)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import position
 import pygame
 import os
 import random
@@ -13,8 +14,7 @@ pygame.display.set_caption(GAME_NAME)
 
 #Timing Values
 FPS = 60
-PLAYER_VEL = 5
-BALL_VEL = 3
+BALL_VEL = 10
 
 #COLORS
 WHITE = (255,255,255)
@@ -27,6 +27,7 @@ INDIGO = (20,10,40)
 ORANGE = (255, 165, 0)
 
 #Dimensions
+BALL_SCALE = 20
 CROSSHAIR_SCALE = 50
 CROSSHAIR_LINE_LENGTH = 300
 MIN_ANGLE = 25
@@ -37,6 +38,7 @@ ENEMY_WIDTH, ENEMY_HEIGHT = 100,100
 # Ex: PLAYER_SPRITE = pygame.image.load(os.path.join('path','to','image'))
 BG_TILE = pygame.image.load(os.path.join('Images','grass.jpg'))
 CROSSHAIR_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images','crosshair_1.png')),(CROSSHAIR_SCALE, CROSSHAIR_SCALE))
+DEFAULT_BALL_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images','white_ball.png')),(BALL_SCALE, BALL_SCALE))
 
 #AUDIO
 # Ex: PLAYER_HIT_SOUND = pygame.mixer.Sound(os.path.join('path', 'to', 'audio'))
@@ -48,6 +50,53 @@ CROSSHAIR_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('Images'
 #TEXT
 # WINNER_FONT = pygame.font.SysFont('Ariel', 80)
 # TEXT_PADDING = 15
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, spawn_x, spawn_y, launch_timer):
+        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
+        self.image = DEFAULT_BALL_IMAGE
+        self.rect = pygame.Rect(spawn_x, spawn_y, BALL_SCALE, BALL_SCALE)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.launch_timer = launch_timer
+        self.x = spawn_x
+        self.y = spawn_y
+        self.direction_x = 1 #for LEFT: 1, for RIGHT: -1
+        self.direction_y = 1 #for UP: 1, for DOWN: -1
+
+    def calculate_movement(self,crosshair_angle):
+        '''
+        Calculate the distance required to move on x and y planes using BALL_VEL and crosshair_angle. 
+        Then calculate the new x,y coordinates by finding the difference from the old coordinates and the distance to travel. 
+        
+        EX:
+        BALL_VEL = 10           <--- This is the hypotenuse
+        crosshair_angle = 50    <--- Angle in degrees
+
+        x_movement -> cos(50) = x/10 -> cos(50)*10 = x -> x = 6.427876096865393
+        y_movement -> sin(50) = x/10 -> sin(50)*10 = x -> x = 7.66044443118978
+
+        self.x = self.x - x_movement * self.direction_x  <--- self.direction_x (LEFT = 1, for RIGHT = -1)
+        self.y = self.y - y_movement * self.direction_y  <--- self.direction_y (UP = 1, for DOWN = -1)
+        '''
+        print('ANGLE')
+        print(crosshair_angle)
+        print('X Before')
+        print(self.x)
+        print('Y Before')
+        print(self.y)
+        x_movement = math.cos(math.degrees(crosshair_angle))*BALL_VEL
+        y_movement = math.sin(math.degrees(crosshair_angle))*BALL_VEL
+        self.x = self.x - x_movement*self.direction_x
+        self.y = self.y - y_movement*self.direction_y
+        print('X Movement')
+        print(x_movement)
+        print('X After')
+        print(self.x)
+        print('Y Movement')
+        print(y_movement)
+        print('Y After')
+        print(self.y)
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, enemy_image: pygame.Surface, spawn_x, spawn_y, hp=10):
@@ -68,12 +117,19 @@ class Game(object):
         self.anchor_y = ANCHOR_Y
         self.crosshair_x = self.anchor_x
         self.crosshair_y = self.anchor_y
+        self.crosshair_angle = 90
+        self.balls = []
+        self.shooting = False
         self.game_setup()
         self.main()
 
     def game_setup(self): 
         '''Resets game values after restart'''
-        pass
+        self.anchor_x = WIDTH//2
+        self.anchor_y = ANCHOR_Y
+        self.shooting = False
+        ball = Ball(self.anchor_x,self.anchor_y,0)
+        self.balls = [ball]
 
     def handle_collisions(self):
         '''
@@ -100,7 +156,16 @@ class Game(object):
         pygame.draw.rect(WIN, rgb_color, rect_obj)
         '''
         self.render_tile_background(BG_TILE)
-        self.draw_ball_crosshair()
+        if self.shooting == True:
+            #Draw ball moving at BALL_VEL towards crosshair
+            for ball in self.balls:
+                ball.launch_timer -= 1
+                if ball.launch_timer < 0:
+                    ball.calculate_movement(self.crosshair_angle)
+                    WIN.blit(ball.image,(ball.x-BALL_SCALE//2,ball.y-BALL_SCALE//2))
+        else:
+            self.draw_ball_crosshair()
+
         pygame.display.update()
 
     def main(self):
@@ -113,6 +178,8 @@ class Game(object):
                     run = False
                     pygame.quit()
 
+                if event.type == pygame.MOUSEBUTTONDOWN: #Launch Balls
+                    self.shooting = True            
             self.handle_collisions()
             self.draw_window()
 
@@ -130,20 +197,25 @@ class Game(object):
         cursor_distance_x = abs(self.anchor_x-mouse_x) #ADJACENT side
         cursor_distance_y = abs(self.anchor_y-mouse_y) #OPPOSITE side
         if cursor_distance_x == 0:
-            angle = 90
+            self.crosshair_angle = 90
         else:
-            angle = math.degrees(math.atan(cursor_distance_y/cursor_distance_x)) # Angle = tan(opposite/adjacent)
-            if angle < 30:
-                angle = 30
+            self.crosshair_angle = math.degrees(math.atan(cursor_distance_y/cursor_distance_x)) # Angle = tan(opposite/adjacent)
+            if self.crosshair_angle < 30:
+                self.crosshair_angle = 30
             else:
-                angle = int(angle)
-        if angle >= MIN_ANGLE and self.anchor_y>mouse_y: 
+                self.crosshair_angle = int(self.crosshair_angle)
+        if self.crosshair_angle >= MIN_ANGLE and self.anchor_y>mouse_y: 
             self.crosshair_x = mouse_x
             self.crosshair_y = mouse_y
-        max_x = int(math.cos(math.radians(angle))*CROSSHAIR_LINE_LENGTH)
-        max_y = int(math.sin(math.radians(angle))*CROSSHAIR_LINE_LENGTH)
+        max_x = int(math.cos(math.radians(self.crosshair_angle))*CROSSHAIR_LINE_LENGTH)
+        max_y = int(math.sin(math.radians(self.crosshair_angle))*CROSSHAIR_LINE_LENGTH)
         if mouse_x > self.anchor_x:
             max_x = max_x*-1
+            for ball in self.balls:
+                ball.direction_x = -1
+        else:
+            for ball in self.balls:
+                ball.direction_x = 1
         WIN.blit(CROSSHAIR_IMAGE,((self.anchor_x-max_x)-CROSSHAIR_SCALE//2,(self.anchor_y-max_y)-CROSSHAIR_SCALE//2))
         pygame.draw.line(WIN,WHITE,(self.anchor_x,self.anchor_y),(self.anchor_x-max_x,self.anchor_y-max_y))
 
